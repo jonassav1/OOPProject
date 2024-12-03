8f40c33c9955d8e067d5bead14a42cbe322cdc2f
Jonas Savickas <jonsavickas@gmail.com>
2024-12-03 21:36:51 +0200

    question progress2

--- tool/questions.py
+++ tool/questions.py
@@ -1,4 +1,4 @@
-
+import csv
 
 class Questions:
     def __init__(self):
@@ -6,13 +6,14 @@ class Questions:
         self.free_form_questions = []
 
     def add_questions(self):
+        file_name = "qquestion.csv"
         while True:
             print("\n\nChoose what type of questions you would like to add!")
             print("1. Quizz questions!")
             print("2. Free-form text questions!")
             print("3. Go back to main menu.")
             try:
-                optionQ = int(input("Choose type of the question or go back to main menu: "))
+                optionQ = int(input("\nChoose type of the question or go back to main menu: "))
                 if optionQ == 1:
                     self.add_quiz_questions()
                 elif optionQ == 2:
@@ -20,21 +21,21 @@ class Questions:
                 elif optionQ == 3:
                     break
                 else:
-                    print("You must enter a number between 1 and 3") 
+                    print("\nYou must enter a number between 1 and 3") 
             except ValueError:
                 print("Invalid input")
 
     def add_quiz_questions(self):
         try:
             print("cia parasyti patikrinima ar failas egisstuoja ")
-            question = input("Enter your quiz type question: ")
+            question = input("\nEnter your quiz type question: \n")
             answer1 = input("Enter first answer: ")
             answer2 = input("Enter second answer: ")
             answer3 = input("Enter third answer: ")
             answer4 = input("Enter fourth answer: ")
             while True:
                 try:
-                    correct_answer = int(input("Select which answer is correct 1, 2, 3, 4:"))
+                    correct_answer = int(input("\nSelect which answer is correct 1, 2, 3, 4:"))
                     if correct_answer in [1, 2, 3, 4]:
                         correct_answer_is = [answer1, answer2, answer3, answer4][correct_answer - 1]
                         break
@@ -42,16 +43,18 @@ class Questions:
                         print("\nYou must select correct asnwer from your inputs!")
                         print(f"{answer1} as 1 \n {answer2} as 2 \n {answer3} as 3 \n {answer4} as 4 ")
                 except ValueError:
-                    print("Invalid input")
+                    print("\nInvalid input")
             self.quiz_questions.append({
                 "question" : question,
                 "answers" : [answer1, answer2, answer3, answer4],
                 "correct_answer" : correct_answer_is
             })
+            with open("qquestion.csv", "a", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerow([question, answer1, answer2, answer3, answer4, correct_answer_is])
             print("Question added.")
-            print(self.quiz_questions)
         except Exception as e:
-            print(f"this file doesnt exist {e}")
+            print(f"\nthis file doesnt exist {e}")
 
     def add_free_from_questions(self):
         ...
