2e21ca47868a2b9121356c37a62f2317d6c8b900
Jonas Savickas <jonsavickas@gmail.com>
2024-12-07 18:56:46 +0200

    done

--- tool/stats.py
+++ tool/stats.py
@@ -9,6 +9,8 @@ class Stats:
         self.show_statistics("qquestion.csv")
         print("\nStatistics for the free-form questions:")
         self.show_statistics("ffqquestions.csv")
+        print("\nStatistics from tests!")
+        self.show_test_stats("result.txt")
 
     def show_statistics(self, file_name):
         with open(file_name, "r") as f:
@@ -21,4 +23,10 @@ class Stats:
                     precentage = (correct_answers / answered) * 100
                 else: 
                     precentage = 0 
-                print(f"ID: {row[0]}, Active: {row[1]}, Question: {row[3]}, Answer: {row[2]}, Times asked: {row[-3]}, Correctly answered rate: {precentage}%")
+                print(f"In practice, ID: {row[0]}, Active: {row[1]}, Question: {row[3]}, Answer: {row[2]}, Times asked: {row[-3]}, Correctly answered rate: {precentage}%")
+
+    def show_test_stats(self, file_name):
+        with open(file_name, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(f"{row}")
